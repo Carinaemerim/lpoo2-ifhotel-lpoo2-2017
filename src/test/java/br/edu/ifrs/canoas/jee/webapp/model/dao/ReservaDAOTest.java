55a5fb516a46f2464ff734781e76f897f8d21f0c
FelipeEmerim <lip.emerim@gmail.com>
2017-11-22 11:18:14 -0200

    Inicio Testes ReservaDAO

---
+++ src/test/java/br/edu/ifrs/canoas/jee/webapp/model/dao/ReservaDAOTest.java
@@ -0,0 +1,45 @@
+package br.edu.ifrs.canoas.jee.webapp.model.dao;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import br.edu.ifrs.canoas.jee.webapp.model.entity.Reserva;
+
+@RunWith(Arquillian.class)
+public class ReservaDAOTest {
+
+		@Inject
+		ReservaDAO reservaDAO;
+		
+		@Deployment
+	    public static Archive<?> createTestArchive() {
+		    return ShrinkWrap.create(WebArchive.class, "test.war")
+	                .addClasses(ReservaDAO.class)
+	                .addPackages(true, "br.edu.ifrs.canoas.jee.webapp")
+	                .addPackages(true, "org.apache.commons")
+	                .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+	                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	    }
+		
+		@Test
+		public void testa_a_persistencia_da_reserva_em_branco () {	
+			Reserva reserva = new Reserva();
+			reserva.setValor(198.90);
+			reservaDAO.insere(reserva);
+			assertNotNull(reserva.getId());
+		
+		}
+		
+		
+
+	}
