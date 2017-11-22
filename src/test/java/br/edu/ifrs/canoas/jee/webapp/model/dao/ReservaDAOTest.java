04e883339a5a98b2630aac53df133ab4a5bfadbb
FelipeEmerim <lip.emerim@gmail.com>
2017-11-22 11:19:07 -0200

    Testes ReservaDAO

--- src/test/java/br/edu/ifrs/canoas/jee/webapp/model/dao/ReservaDAOTest.java
+++ src/test/java/br/edu/ifrs/canoas/jee/webapp/model/dao/ReservaDAOTest.java
@@ -7,28 +7,22 @@ import javax.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import br.edu.ifrs.canoas.jee.webapp.model.entity.Reserva;
 
 @RunWith(Arquillian.class)
-public class ReservaDAOTest {
+public class ReservaDAOTest extends BaseDAOTest{
 
 		@Inject
 		ReservaDAO reservaDAO;
 		
 		@Deployment
 	    public static Archive<?> createTestArchive() {
-		    return ShrinkWrap.create(WebArchive.class, "test.war")
-	                .addClasses(ReservaDAO.class)
-	                .addPackages(true, "br.edu.ifrs.canoas.jee.webapp")
-	                .addPackages(true, "org.apache.commons")
-	                .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
-	                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+		    return createTestArchive().
+	                .addClasses(ReservaDAO.class);
 	    }
 		
 		@Test
