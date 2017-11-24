41dd0ac4207b367ea2ec876b5de14816a871c11d
FelipeEmerim <lip.emerim@gmail.com>
2017-11-24 09:45:07 -0200

    Criados os testes para BaseDAO, adicionada a anotação @Stateless em PessoaFisicaDAO, criado o teste de ReservaDAO

--- src/main/java/br/edu/ifrs/canoas/jee/webapp/model/dao/PessoaFisicaDAO.java
+++ src/main/java/br/edu/ifrs/canoas/jee/webapp/model/dao/PessoaFisicaDAO.java
@@ -2,9 +2,15 @@ package br.edu.ifrs.canoas.jee.webapp.model.dao;
 
 import java.util.List;
 
+import javax.ejb.Stateless;
+
 import br.edu.ifrs.canoas.jee.webapp.model.entity.PessoaFisica;
 
+@Stateless
 public class PessoaFisicaDAO extends BaseDAO<PessoaFisica, Long> {
+	
+	private static final long serialVersionUID = -1691949096247206266L;
+
 	public PessoaFisica buscaPorDocumento(String documento){
 		List<PessoaFisica> pfs;
 		
--- src/test/java/br/edu/ifrs/canoas/jee/webapp/model/dao/ReservaDAOTest.java
+++ src/test/java/br/edu/ifrs/canoas/jee/webapp/model/dao/ReservaDAOTest.java
@@ -7,22 +7,28 @@ import javax.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import br.edu.ifrs.canoas.jee.webapp.model.entity.Reserva;
 
 @RunWith(Arquillian.class)
-public class ReservaDAOTest extends BaseDAOTest{
+public class ReservaDAOTest{
 
 		@Inject
 		ReservaDAO reservaDAO;
 		
 		@Deployment
 	    public static Archive<?> createTestArchive() {
-		    return createTestArchive().
-	                .addClasses(ReservaDAO.class);
+			 return ShrinkWrap.create(WebArchive.class, "test.war")
+		                .addClasses(ReservaDAO.class)
+		                .addPackages(true, "br.edu.ifrs.canoas.jee.webapp")
+		                .addPackages(true, "org.apache.commons")
+		                .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+		                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
 	    }
 		
 		@Test
@@ -35,5 +41,4 @@ public class ReservaDAOTest extends BaseDAOTest{
 		}
 		
 		
-
-	}
+}
